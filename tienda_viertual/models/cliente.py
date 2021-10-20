fae6bb7794289b18ea63bd7a8a3a8f4a655f1190
christianbohorquez <arturobohorquez@gmail.com>
2021-10-19 20:31:07 -0500

    Tienda virtual de django corrigiendo

--- tienda_viertual/models/cliente.py
+++
@@ -1,6 +0,0 @@
-from tienda_viertual import db
-class Cliente(db.Model):
-    __tablename__ = "models"
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(32), nullable=False)
-    cedula = db.Column(db.String(16), nullable=False)
